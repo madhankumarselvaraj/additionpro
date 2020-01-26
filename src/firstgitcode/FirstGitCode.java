d07597a23d877218968ebd4a70903075b7e5f27d
Madhankumar Selvaraj <Madhankumar Selvaraj@chakranetwork.com>
2020-01-26 10:51:03 +0530

    java code changed

---
+++ src/firstgitcode/FirstGitCode.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstgitcode;
+
+/**
+ *
+ * @author Madhankumar Selvaraj
+ */
+public class FirstGitCode {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        int a = 5, b = 6;
+        int cc = a + b;
+        System.out.println("Add : " + cc);
+
+    }
+
+}
